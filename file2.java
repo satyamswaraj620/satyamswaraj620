2c25e210710acae5d23218c62f3d2f113b8da4bf
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-05-31 01:47:48 +0530

    uplodaded upto file2

---
+++ file2.java
@@ -0,0 +1,84 @@
+//---comment -> yaha se gadbad hai dekhlo
+//-- yaha  pe se kam susur krna tha 
+/*    multi line comment */ 
+
+// -  single line comment
+
+/*
+c ---    .c               (extension)   .txt = textfile    .java = javafile  phopts -- jpg 
+                            png  -- image file    music file - mp3  
+cpp   .cpp
+
+java  .java
+python - - .py
+javascript - ---  .js
+
+1  1 oil --- 
+10  sugar 
+
+very important  - thing is that the name of the file should be same as the name of the class which is public.
+
+container dabba ;
+
+numbers === 233, 3,2323, 2 ,3333333333333333333,   === (int  === integer )  === size(4 bytes)
+
+, letter ==  s , d A to z ==  (char  ==   characters) ==== size(2bytes )
+ name  === shivam , swraj ,satyam, shivamswrrajsatysjs ===
+
+ decimal == 4.4 ,4.6 ,75433333.833333333 = === (double) == size(8 bytes);
+ 
+ word = shivam  == (string ) == size(4)
+ sentence  == =  This is my name  my name iss shivam ===
+
+ true or false ====   boolean (bool)  true ==  1 , false == 0
+
+
+             -------------------- datatypes---------------
+ integer(int)
+string,
+character (char),
+double ,,,,,
+*******
+float,short,long, 
+   
+compile --- 
+
+run -- 
+ 
+() - parenthesis;
+ {} -- curly braket--braces;
+[] - square bracket;
+
+satyam() ---  function
+main() -- main function --  ()
+add() -- add function
+
+
+ */
+
+
+class file2{
+
+public static void main(String[]  args){ 
+    
+    
+       
+     /*    System.out.print(" ");  
+System.out.print(" ");  
+
+System.out.print(" ");     
+
+System.out.print(" ");  
+
+System.out.print(" ");    */
+
+
+System.out.print(" satyam saraj ");
+
+
+
+
+
+}
+
+}
