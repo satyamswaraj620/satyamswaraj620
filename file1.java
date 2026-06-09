2bc52860d07e940d9be8cba553c845678a903cb7
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-06-10 02:48:42 +0530

    updated

--- file1.java
+++ file1.java
@@ -1,10 +1,10 @@
 //Hello world code
 
 
-
+  
 import java.util.Scanner;
 class file1{
-    public static void main(String[] args){
+        public static void main(String[] args){
         System.out.println("HEllo world");
     }
 } 
