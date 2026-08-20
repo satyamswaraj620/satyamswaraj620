61a8b8b79424ffb5e8969482307b93b737f8cc0d
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-08-20 19:49:45 +0530

    Remove commented package declaration

--- NOTESLEC04CWH.java
+++ NOTESLEC04CWH.java
@@ -1,6 +1,6 @@
 //package com.company;
 import java.util.Scanner;
-
+    
 public class NOTESLEC04CWH {
     public static void main(String[] args) {
         System.out.println("Taking Input From the User");
