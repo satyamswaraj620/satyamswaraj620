66c80a2a8f9980c1b6f5bca729c8db92ce91b339
Satyam Swaraj <satyamswaraj62@gmail.com>
2026-09-03 01:19:19 +0530

    Fix formatting and improve output in practicefile.java

--- practicefile.java
+++ practicefile.java
@@ -3,7 +3,7 @@
 int standard = 12;
 String name = "Satyam swaraj";
 String place = "patna";
-
+        
 System.out.println("My name is "+name);
 System.out.println("i study in class "+" "+standard);
 System.out.print("i live in "+" "+place);
